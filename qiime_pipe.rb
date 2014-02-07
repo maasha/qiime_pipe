8a59ac88fd1dcd85e46496f41246545422713a7e
Martin Hansen <maasha@orsted.dna.ku.dk>
2014-02-07 14:49:54 +0100

    added --restart feature

--- qiime_pipe.rb
+++ qiime_pipe.rb
@@ -8,75 +8,99 @@ ARGV << "-h" if ARGV.empty?
 
 cmd_init = File.basename($0) + " " + ARGV.join(" ")
 
-options = {}
+def option_parser(args)
+  options = {}
 
-OptionParser.new do |opts|
-  opts.banner = "Usage: #{File.basename(__FILE__)} [options]"
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{File.basename(__FILE__)} [options]"
 
-  opts.on("-h", "--help", "Display this screen" ) do
-    $stderr.puts opts
-    exit
+    opts.on("-h", "--help", "Display this screen" ) do
+      $stderr.puts opts
+      exit
+    end
+
+    opts.on("--restart <log file>", "Restart job") do |o|
+      options[:restart] = o
+    end
+
+    opts.on("-s", "--file_sff <file>", String, "SFF file to process") do |o|
+      options[:file_sff] = o
+    end
+
+    opts.on("-i", "--dir_illumina <dir>", String, "Illumina directory to process") do |o|
+      options[:dir_illumina] = o
+    end
+
+    opts.on("-m", "--file_map <file>", String, "Mapping file to process") do |o|
+      options[:file_map] = o
+    end
+
+    opts.on("-r", "--remote_map <key>", String, "Remote mapping file to process") do |o|
+      options[:remote_map] = o
+    end
+
+    opts.on("-o", "--dir_out <dir>", String, "Output directory") do |o|
+      options[:dir_out] = o
+    end
+
+    opts.on("-f", "--force", "Force overwrite log file and output directory") do |o|
+      options[:force] = o
+    end
+
+    opts.on("-d", "--denoise", "Denoise data") do |o|
+      options[:denoise] = o
+    end
+
+    opts.on("-c", "--chimera", "Chimera filter data") do |o|
+      options[:chimera] = o
+    end
+
+    opts.on("--trim_primers", "Trim primers from reads prior to assembly") do |o|
+      options[:trim_primers] = o
+    end
+
+    opts.on("-M", "--catagory <string>", String, "Mapping catagory (mapping file column label") do |o| 
+      options[:catagory] = o
+    end
+
+    options[:chimera_db] = Qiime::DEFAULT_CHIMERA_DB
+    opts.on("-D", "--chimera_db <file>", String, "Chimere database (#{Qiime::DEFAULT_CHIMERA_DB})") do |o|
+      options[:chimera_db] = o || Qiime::DEFAULT_CHIMERA_DB
+    end
+
+    options[:barcode_size] = Qiime::DEFAULT_BARCODE_SIZE
+    opts.on("-b", "--barcode_size <int>", Integer, "Size of barcodes used (#{Qiime::DEFAULT_BARCODE_SIZE})") do |o|
+      options[:barcode_size] = o
+    end
+
+    options[:cpus] = Qiime::DEFAULT_CPUS
+    opts.on("-C", "--cpus <int>", Integer, "Number of CPUs to use (#{Qiime::DEFAULT_CPUS})") do |o|
+      options[:cpus] = o
+    end
+
+    opts.on("-e", "--email <string>", String, "Send email alert") do |o| 
+      options[:email] = o
+    end
+  end.parse!(args)
+
+  options
+end
+
+options = option_parser(ARGV)
+
+if options[:restart]
+  raise OptionParser::InvalidOption, "no such file: #{options[:restart]}" unless File.file? options[:restart]
+
+  File.open(options[:restart]) do |ios|
+    line = ios.gets
+
+    if line[0] == '#'
+      options = option_parser(line[1 .. -1].split " ")
+    else
+      raise "Failed to parse log file: #{options[:restart]}"
+    end
   end
-
-  opts.on("-s", "--file_sff <file>", String, "SFF file to process") do |o|
-    options[:file_sff] = o
-  end
-
-  opts.on("-i", "--dir_illumina <dir>", String, "Illumina directory to process") do |o|
-    options[:dir_illumina] = o
-  end
-
-  opts.on("-m", "--file_map <file>", String, "Mapping file to process") do |o|
-    options[:file_map] = o
-  end
-
-  opts.on("-r", "--remote_map <key>", String, "Remote mapping file to process") do |o|
-    options[:remote_map] = o
-  end
-
-  opts.on("-o", "--dir_out <dir>", String, "Output directory") do |o|
-    options[:dir_out] = o
-  end
-
-  opts.on("-f", "--force", "Force overwrite log file and output directory") do |o|
-    options[:force] = o
-  end
-
-  opts.on("-d", "--denoise", "Denoise data") do |o|
-    options[:denoise] = o
-  end
-
-  opts.on("-c", "--chimera", "Chimera filter data") do |o|
-    options[:chimera] = o
-  end
-
-  opts.on("--trim_primers", "Trim primers from reads prior to assembly") do |o|
-    options[:trim_primers] = o
-  end
-
-  opts.on("-M", "--catagory <string>", String, "Mapping catagory (mapping file column label") do |o| 
-    options[:catagory] = o
-  end
-
-  options[:chimera_db] = Qiime::DEFAULT_CHIMERA_DB
-  opts.on("-D", "--chimera_db <file>", String, "Chimere database (#{Qiime::DEFAULT_CHIMERA_DB})") do |o|
-    options[:chimera_db] = o || Qiime::DEFAULT_CHIMERA_DB
-  end
-
-  options[:barcode_size] = Qiime::DEFAULT_BARCODE_SIZE
-  opts.on("-b", "--barcode_size <int>", Integer, "Size of barcodes used (#{Qiime::DEFAULT_BARCODE_SIZE})") do |o|
-    options[:barcode_size] = o
-  end
-
-  options[:cpus] = Qiime::DEFAULT_CPUS
-  opts.on("-C", "--cpus <int>", Integer, "Number of CPUs to use (#{Qiime::DEFAULT_CPUS})") do |o|
-    options[:cpus] = o
-  end
-
-  opts.on("-e", "--email <string>", String, "Send email alert") do |o| 
-    options[:email] = o
-  end
-end.parse!
+end
 
 unless options[:file_map] or options[:remote_map]
   raise OptionParser::MissingArgument, "--file_map or --remote_map"
